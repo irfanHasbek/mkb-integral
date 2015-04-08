59b8caf9702fd21e71b7dd72e808ef392439a63e
abuzerdurmus <abuzer.durmus02@gmail.com>
2015-04-08 11:38:47 +0300

    musteri iskonto 2

--- views/js/pages-js/CustomerDiscountPage.js
+++ views/js/pages-js/CustomerDiscountPage.js
@@ -15,12 +15,13 @@ function formHandlers(){
             var td3=$("<td>"+resp.response.productGroupName+"</td>");
             var td5=$("<td><span>% </span>"+resp.response.percent+"</td>");
             var td6=$("<td>"+resp.response.owner+"</td>");
-            var btnSil='<button class="btn btn-danger btn-sm sil"><i class="fa fa-trash-o"></i></button>';
+            var btnSil='<button class="btn btn-danger sil"><i class="fa fa-trash-o"></i></button>';
             var td4=$("<td st id="+resp.response._id+">"+btnSil+"</td>");
             tr.append(td1);tr.append(td2);tr.append(td3);tr.append(td5);tr.append(td6);tr.append(td4);
             $(".discountTable tbody").last().append(tr);
             $("input[type='text']").val("");
             $("input[type='number']").val("");
+            $("#slctProductGroup").val("");
         }else{
             alertify.error("İşlem başarısız.");
         }
