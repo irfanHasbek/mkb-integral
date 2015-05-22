b1fac40353797caf33e9e4d65b313cf114b61f0a
abuzerdurmus <abuzer.durmus02@gmail.com>
2015-05-22 12:39:26 +0300

    register

--- views/js/pages-js/CustomerRegisterPage.js
+++ views/js/pages-js/CustomerRegisterPage.js
@@ -2,7 +2,15 @@ function clickHandlers(){
     
 }
 function formHandlers(){
-    
+    $("#frmCustomerRegister").ajaxForm(function(response){
+        if(!response.state){
+            alertify.error("hata!! lütfen tekrar deneyiniz.");
+            return;
+        }
+        $("input[type='text']").val("");
+        $("textarea[type='text']").text("");
+        alertify.success("Başarıyla eklendi.Bilgileriniz Firmamıza ulaştı.");
+    });
 }
 function otherScripts(){
     
